1b2d5364ac1fb04aefbed425fd068343b4f50dd2
David Jaroš - DJ <david.jaros@chyron.com>
2025-01-15 10:22:55 +0100

    Add project files.

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPF_TestingApp_DJ.Model;
+
+namespace WPF_TestingApp_DJ.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+        private string filterText;
+
+        public MainViewModel()
+        {
+            ClearFilterCommand = new RelayCommand(ClearFilter, CanClearFilter);
+        }
+
+
+        public ICommand ClearFilterCommand { get; }
+
+        public string FilterText
+        {
+            get => filterText;
+            set
+            {
+                if (filterText != value)
+                {
+                    filterText = value;
+                    OnPropertyChanged(nameof(FilterText));
+                }
+            }
+        }
+       
+
+        private void ClearFilter()
+        {
+            FilterText = string.Empty;
+        }
+
+        private bool CanClearFilter()
+        {
+            return !string.IsNullOrEmpty(FilterText);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
